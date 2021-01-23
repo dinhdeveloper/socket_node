4fc4844a6e84244dc7746829e7d5c99433bcd985
dinhdeveloper <dinhtrancntt@gmail.com>
2021-01-23 14:14:06 +0700

    adadad

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const axios = require("axios");
-// const cors = require("cors");
+const cors = require("cors");
 // const request = require("request");
 // const { data } = require("jquery");
 const cron = require("node-cron");
